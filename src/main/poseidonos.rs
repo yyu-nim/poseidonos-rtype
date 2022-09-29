de07c8f7699d22222d3adb7d6db786cdacd4b45a
yyu-nim <lefoot@gmail.com>
2022-09-29 22:37:11 +0900

    migrate POS main over

---
+++ src/main/poseidonos.rs
@@ -0,0 +1,16 @@
+pub struct Poseidonos;
+
+impl Poseidonos {
+    pub fn Init(&self, _args: Vec<&str>) -> i32 {
+        // TODO
+        0
+    }
+
+    pub fn Run(&self) {
+        // TODO
+    }
+
+    pub fn Terminate(&self) {
+        // TODO
+    }
+}
